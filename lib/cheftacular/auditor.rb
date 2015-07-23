b19feae1338103da2422dd3bada141b889b0dd79
Louis Alridge <louis@hiplogiq.com>
2015-07-23 13:04:35 -0500

    fixing issue with auditor not actually removing keys from audit log

--- lib/cheftacular/auditor.rb
+++ lib/cheftacular/auditor.rb
@@ -20,10 +20,18 @@ class Cheftacular
       File.open( @config['filesystem'].current_audit_file_path, "w") { |f| f.write( fetch_audit_data_hash ) }
     end
 
-    def read_audit_cache_file_to_hash ret_hash={}
+    def read_audit_cache_file_to_hash ret_hash={}, options_to_ignore=[]
       ret_hash = Hash.class_eval( File.read( @config['filesystem'].current_audit_file_path ))
       ret_hash['command']   = @options['command']
-      ret_hash['options']   = @options.except(:preferred_cloud, :preferred_cloud_region, :preferred_cloud_image) #TODO load preferred_X options if they are not the default?
+      
+      options_to_ignore << :preferred_cloud        if @options['preferred_cloud'] == @config['cheftacular']['preferred_cloud']
+      options_to_ignore << :preferred_cloud_image  if @options['preferred_cloud_image'] == @config['cheftacular']['preferred_cloud_image']
+      options_to_ignore << :preferred_cloud_region if @options['preferred_cloud_region'] == @config['cheftacular']['preferred_cloud_region']
+      options_to_ignore << :virtualization_mode    if @options['virtualization_mode'] == @config['cheftacular']['virtualization_mode']
+      options_to_ignore << :route_dns_changes_via  if @options['route_dns_changes_via'] == @config['cheftacular']['route_dns_changes_via']
+
+      ret_hash['options']   = @options.dup.delete_if { |key, value| options_to_ignore.include?(key.to_sym) }
+
       ret_hash['arguments'] = ARGV[1..ARGV.length]
 
       ret_hash
