77bb2f49476db7d3c16865fa3abdd79ad8bc9d24
Louis Alridge <louis@hiplogiq.com>
2015-10-03 06:28:43 -0500

    2.8.0, minor fix to initialize_data_bag_contents

--- lib/cheftacular/stateless_actions/initialize_data_bag_contents.rb
+++ lib/cheftacular/stateless_actions/initialize_data_bag_contents.rb
@@ -171,7 +171,7 @@ class Cheftacular
         @config['getter'].get_repo_names_for_repositories.each_key do |repo_name|
           #upgrades pre 2.7.2 to 2.8.0+ for config bag formats TODO DELETE ME (remove after 3.0.0)
           if hash[env]['app_revisions'].has_key?(repo_name) && hash[env]['app_revisions'][repo_name].class.to_s == 'String'
-            hash[env]['app_revisions'][repo_name]['revision'] = hash[env]['app_revisions'][repo_name]
+            hash[env]['app_revisions'][repo_name] = { "revision" => hash[env]['app_revisions'][repo_name] }
           end
 
           unless hash[env]['app_revisions'].has_key?(repo_name)
